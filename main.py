d1aaf60fd7b42253f0528d3a9452f9213d0404c7
Artem <53917645+under-web@users.noreply.github.com>
2021-09-30 15:16:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
+import time
+
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+# TODO:написать фунцию с возвратом цены и сравнивать ее вывод 
 
-ua = UserAgent()
-headers = {'user-agent': ua.firefox}
-url = 'https://www.google.com/search?client=firefox-b-d&q=wtyf+%2Cbnrjbyf'
-r = requests.get(url, headers=headers, )
+while True:
+    ua = UserAgent()
+    headers = {'user-agent': ua.firefox}
+    url = 'https://www.rbc.ru/crypto/currency/btcusd'
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+    mid_price = soup.find('div', class_="chart__subtitle js-chart-value")
+    old_price = mid_price.text.replace(' ', '')
+    time.sleep(1)
+    ua = UserAgent()
+    headers = {'user-agent': ua.firefox}
+    url = 'https://www.rbc.ru/crypto/currency/btcusd'
+    res = requests.get(url, headers=headers)
 
-soup = BeautifulSoup(r.text, 'lxml')
+    soup_1 = BeautifulSoup(r.text, 'lxml')
+    next_mid_price = soup.find('div', class_="chart__subtitle js-chart-value")
+    new_price = mid_price.text.replace(' ', '')
 
-mid_price = soup.find_all(['div', 'span'], "r0bn4c rQMQod")
-for i in mid_price:
-    if '+' in i.text:
-        print(i.text)
-
+    result = (int(old_price[0:6]) - int(new_price[0:6]))
+    if result > 1000:
+        print('Колебание цены ', int(new_price[0:6]))
+    else:
+        print(int(new_price[0:6]))
+        print('разница', result)
